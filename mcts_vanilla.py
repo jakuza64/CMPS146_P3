1e747ec5c699d804f41a4e15da028229605a8008
ghreyes <ghreyes921@gmail.com>
2016-10-15 13:36:06 -0700

    Add files via upload
    Merged code with master and added to end of MCTS

--- mcts_vanilla.py
+++ mcts_vanilla.py
@@ -6,6 +6,27 @@ from math import sqrt, log
 num_nodes = 1000
 explore_faction = 2.
 
+def get_urgent_child(node,identity):
+    n = node.visits
+    maxUCB = 0
+    best_child = None
+    for action,child in node.child_nodes.items():
+        xj = child.wins/child.visits
+        # If opponent
+        if identity != state.player_turn:
+            xj = 1 - xj
+        nj = child.visits
+
+        # Calculate
+        newUCB = xj + explore_faction * (sqrt((2 * log(n)) / (nj)))
+        if newUCB > maxUCB:
+            best_child = child
+            maxUCB = newUCB
+
+    #If none, current node is leaf node
+    return best_child
+    pass
+
 def traverse_nodes(node, state, identity):
     """ Traverses the tree until the end criterion are met.
 
@@ -17,7 +38,13 @@ def traverse_nodes(node, state, identity):
     Returns:        A node from which the next stage of the search can proceed.
 
     """
-    
+    while True:
+        next_node = get_urgent_child(node,identity)
+        if next_node != None:
+            state.apply_move(next_node.parent_action)
+            node = next_node
+        else:
+            return node #,state
     pass
     # Hint: return leaf_node
 
@@ -32,6 +59,11 @@ def expand_leaf(node, state):
     Returns:    The added child node.
 
     """
+    next_action = node.untried_actions.pop()
+    next_node = MCTSNode(parent=node, parent_action=next_action, action_list=node.untried_actions)
+    node.child_nodes[next_action] = next_node
+    state.apply_move
+    return node.child_nodes[next_action]#, state
     pass
     # Hint: return new_node
 
@@ -56,7 +88,8 @@ def rollout(state):
                     rollout_move = choice(rollout_state.legal_moves)
                     rollout_state.apply_move(rollout_move)
     
-               # total_score += outcome(rollout_state.score)
+               #total_score += outcome(rollout_state.score)
+            
     pass
 
 
@@ -96,6 +129,30 @@ def think(state):
 
         # Do MCTS - This is all you!
 
+        #Traversal (Find leaf nodes)
+        #leaf,state = traverse_nodes(node, state, identity_of_bot)
+        leaf = traverse_nodes(node, sampled_game, identity_of_bot)
+
+
+        #Expansion (Add new node with unexplored action)
+        #new_node,state = expand_leaf(leaf,state)
+        new_node = expand_leaf(leaf,sampled_game)
+
+
+        #Rollout (Simulate remainder of game)
+        rollout(sampled_game)
+        
+
+        #Backpropagate (Send result back up tree)
+        backpropagate(new_node, new_node.wins)
+        
+
     # Return an action, typically the most frequently used action (from the root) or the action with the best
     # estimated win rate.
-    return None
+    highest_win_rate = None
+    best_node = None
+    for n in root_node.child_nodes:
+        if(n.wins/n.visits) > highest_win_rate:
+            highest_win_rate = n.wins/n.visits
+            best_node = n
+    return best_node.parent_action
